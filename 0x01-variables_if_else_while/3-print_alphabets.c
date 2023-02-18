e24f434f34d1b0ba213d84cede1c6d14c03ae431
esraaelsharqawy98 <esraaelsharqawy998@gmail.com>
2023-02-17 18:16:44 -0800

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print the letters of the alphabet
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
+char ALPHA[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int i = 0;
+while (i < 26)
+{
+putchar (alpha[i]);
+i++;
+}
+int j = 0;
+while (j < 26)
+{
+putchar (ALPHA[j]);
+j++;
+}
+putchar ('\n');
+return (0);
+}
+
