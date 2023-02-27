59203168aa41a899c6fb85115f629e9d9206019f
esraaelsharqawy98 <esraaelsharqawy998@gmail.com>
2023-02-27 10:45:02 -0800

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 /**
  * reset_to_98 - a function that takes a pointer to an int as parameter
  * and updates the value it points to to 98
- * @n: a pointer as input 
+ * @n: a pointer as input
  */
 void reset_to_98(int *n)
 {
