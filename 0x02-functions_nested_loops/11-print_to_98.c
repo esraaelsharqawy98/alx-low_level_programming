6e1121e985e1f2e5f7948cf8acf3b349bc37a72b
esraa <82068115+esraaelsharqawy98@users.noreply.github.com>
2023-02-27 02:18:43 +0200

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,11 +12,11 @@ for (i = n; i <= 98; i++)
 {
 if (i != 98)
 {
-printf("%d, ", i);
+_putchar(i + '0');
 }
 else if (i == 98)
 {
-printf("%d\n", i);
+_putchar(i + '0');
 }
 }
 }
@@ -26,11 +26,11 @@ for (j = n; j >= 98; j--)
 {
 if (j != 98)
 {
-printf("%d, ", j);
+_putchar(j + '0');
 }
 else if (j == 98)
 {
-printf("%d\n", j);
+_putchar(j + '0');
 }
 }
 }
