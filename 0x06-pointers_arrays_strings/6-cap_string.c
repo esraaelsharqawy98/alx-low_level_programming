5124fa26d18d1e41653f7742eb8e09996a02361c
esraa <82068115+esraaelsharqawy98@users.noreply.github.com>
2023-03-02 23:27:28 +0200

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,7 +10,9 @@ int i = 0;
 while (str[i])
 {
 while (!(str[i] >= 'a' && str[i] <= 'z'))
+{
 i++;
+}
 if (str[i - 1] == ' ' ||
 str[i - 1] == '\t' ||
 str[i - 1] == '\n' ||
@@ -25,7 +27,9 @@ str[i - 1] == ')' ||
 str[i - 1] == '{' ||
 str[i - 1] == '}' ||
 i == 0)
+{
 str[i] -= 32;
+}
 i++;
 }
 return (str);
