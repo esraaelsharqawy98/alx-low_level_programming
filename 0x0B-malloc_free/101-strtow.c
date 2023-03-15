77b62d8f201cc92f67adf5c86b1435f7502f5161
esraa <82068115+esraaelsharqawy98@users.noreply.github.com>
2023-03-16 00:06:18 +0200

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,63 +1,59 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
- * strtow - concatenates all the arguments of your program
- *@str: string
- *@av: arguments
- * Return: a pointer to a new string
+ * **strtow - splits a string into words
+ * @str: string to split
+ * Return: pointer to array of strings, or NULL if fail
  */
 char **strtow(char *str)
 {
-int i = 0, w = 0, j = 0, k, count, m, wordf;
-char **p, *x;
-count = 0;
-if (*str == '\0' || str == NULL)
+char **a;
+int i, k, n, word_count = 0, word_len = 0, current_word = 0;
+if (str == NULL || *str == '\0')
 {
-return (NULL); }
-for (i = 0; str[i] != '\0'; i++)
-{
-if (str[i] == ' ' && (str[i + 1] != ' ' || str[i + 1] == '\0'))
-{
-w++; }
-}
-p = (char **)malloc((w + 1) * sizeof(char *));
-if (p == NULL)
-{
-return (NULL); }
-for (wordf = 0; str[wordf] && j <= w; wordf++)
-{
-count = 0;
-if (str[wordf] != ' ')
-{
-for (i = wordf ; str[i] != '\0'; i++)
-{
-if (str[i] == ' ')
-{
-break; }
-count++;
-}
-*(p + j) = (char *)malloc((count + 1) * sizeof(char));
-if (*(p + j) == NULL)
-{
-for (k = 0; k <= j; k++)
-{
-x = p[k];
-free(x);
-}
-free(p);
 return (NULL);
 }
-for (m = 0; wordf < i; wordf++)
+for (i = 0; *(str + i) != '\0'; i++)
 {
-p[j][m] = str[wordf];
-m++;
+if (*(str + i) != ' ')
+{
+word_count++; }
+while (*(str + i) != ' ' && *(str + i))
+{
+i++; }
+if (!*(str + i))
+{
+break; }
 }
-p[j][m] = '\0';
-j++;
+if (word_count == 0)
+{
+return (NULL); }
+a = malloc(sizeof(char *) * (word_count + 1));
+if (a == NULL)
+{
+return (NULL); }
+for (i = 0, k = i; current_word < word_count; i++, word_len = 0, k = i)
+{
+if (*(str + i) == ' ')
+{
+continue; }
+while (*(str + k) != ' ' && *(str + k++))
+{
+word_len++; }
+a[current_word] = malloc(sizeof(char) * word_len + 1);
+if (!a[current_word])
+{
+while (current_word-- >= 0)
+{
+free(a[current_word]); }
+free(a);
+return (NULL);
 }
+for (n = 0; i < k; i++, n++)
+{
+a[current_word][n] = *(str + i); }
+a[current_word++][n] = '\0';
 }
-p[j] = NULL;
-return (p);
+a[word_count] = NULL;
+return (a);
 }
