6a1505f64d79e6dd370a27367fdc956757be95ac
esraa <82068115+esraaelsharqawy98@users.noreply.github.com>
2023-03-17 03:24:11 +0200

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,83 +1,47 @@
-#include <stdlib.h>
-#include "main.h"		
+#include "main.h"
 /**
- * *_realloc - reallocates a memory block using malloc and free
- * @ptr: pointer to the memory previsouly allocated by malloc
- * @old_size: size of the allocated memory for ptr
- * @new_size: new size of the new memory block
- * Return: pointer to the newly allocated memory block		
- */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)		
+  * _realloc - ...
+  * @ptr: ...
+  * @old_size: ...
+  * @new_size: ...
+  *
+  * Return: ...
+  */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *ptr1;
-		
-	char *old_ptr;
-		
+	char *nptr;
 	unsigned int i;
-		
 
-		
 	if (new_size == old_size)
-		
 		return (ptr);
-		
 
-		
-	if (new_size == 0 && ptr)
-		
+	if (ptr == NULL)
 	{
-		
-		free(ptr);
-		
+		nptr = malloc(new_size);
+
+		if (nptr == NULL)
+			return (NULL);
+
+		return (nptr);
+	}
+	else
+	{
+		if (new_size == 0)
+		{
+			free(ptr);
+			return (NULL);
+		}
+	}
+	nptr = malloc(new_size);
+
+	if (nptr == NULL)
 		return (NULL);
-		
-	}
-		
 
-		
-	if (!ptr)
-		
-		return (malloc(new_size));
-		
-
-		
-	ptr1 = malloc(new_size);
-		
-	if (!ptr1)
-		
-		return (NULL);
-		
-
-		
-	old_ptr = ptr;
-		
-
-		
-	if (new_size < old_size)
-		
+	for (i = 0; i < old_size && i < new_size; i++)
 	{
-		
-		for (i = 0; i < new_size; i++)
-		
-			ptr1[i] = old_ptr[i];
-		
+		nptr[i] = ((char *) ptr)[i];
 	}
-		
 
-		
-	if (new_size > old_size)
-		
-	{
-		
-		for (i = 0; i < old_size; i++)
-		
-			ptr1[i] = old_ptr[i];
-		
-	}
-		
-
-		
 	free(ptr);
-		
-	return (ptr1);		
+	return (nptr);
 }
