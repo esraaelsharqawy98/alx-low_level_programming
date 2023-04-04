877b90899cbbf271cbe72b266c3798cfe0d9f7f3
esraa <82068115+esraaelsharqawy98@users.noreply.github.com>
2023-04-04 23:09:38 +0200

    Update 102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 /**
- * free_listint_safe - frees a linked list
- * @h: pointer to the first node in the linked list
- * Return: number of elements in the freed list
- */
+* free_listint_safe - frees a linked list
+* @h: pointer to the first node in the linked list
+* Return: number of elements in the freed list
+*/
 size_t free_listint_safe(listint_t **h)
 {
 size_t len = 0;
@@ -32,9 +32,6 @@ len++;
 break;
 }
 }
-
 *h = NULL;
-
 return (len);
-}
 }
